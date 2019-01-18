af634fd6d86ba71dfac2e66907dd85d1e09ded91
Clemens Park <clemensp@nulogy.com>
2019-01-18 17:05:22 -0500

    eslint fixes

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,11 @@
-export { default as Box } from './Box/Box'
-export { default as Button } from './Button/Button'
-export { default as PrimaryButton } from './Button/PrimaryButton'
-export { default as DangerButton } from './Button/DangerButton'
-export { default as QuietButton } from './Button/QuietButton'
-export { default as Flex } from './Flex/Flex'
-export { default as Text } from './Type/Text'
-export { default as Link } from './Link/Link'
-export { default as Icon } from './Icon/Icon'
-export * from './Type/Headings'
-export { default as ThemeProvider } from './ThemeProvider/ThemeProvider'
+export { default as Box } from "./Box/Box";
+export { default as Button } from "./Button/Button";
+export { default as PrimaryButton } from "./Button/PrimaryButton";
+export { default as DangerButton } from "./Button/DangerButton";
+export { default as QuietButton } from "./Button/QuietButton";
+export { default as Flex } from "./Flex/Flex";
+export { default as Text } from "./Type/Text";
+export { default as Link } from "./Link/Link";
+export { default as Icon } from "./Icon/Icon";
+export * from "./Type/Headings";
+export { default as ThemeProvider } from "./ThemeProvider/ThemeProvider";
