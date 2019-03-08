984da952a49517fb5b8fc6f9f8bf4cbbb932c49a
Christiaan Oostenbrug <christiaan.oostenbrug@gmail.com>
2019-03-08 16:45:13 -0500

    adds tests on arrow navigation and menu closing

--- components/cypress/integration/SubMenu.spec.js
+++ components/cypress/integration/SubMenu.spec.js
@@ -18,4 +18,37 @@ describe("NavBar Menu", () => {
       expect($el).to.contain("Customers");
     });
   });
+
+  it("closes the menu on esc key", () => {
+    cy.contains("Dashboard").click();
+    cy.contains("Customers").type("{esc}");
+
+    expect(cy.contains("Customers").should("not.exist"));
+  });
+
+  it("focuses the second sub menu item after opening and then pressing down", () => {
+    cy.contains("Dashboard").click();
+
+    expect(cy.contains("Customers")).to.exist;
+    cy.focused().then($el => {
+      expect($el).to.contain("Customers");
+    });
+    cy.contains("Customers").type("{downarrow}");
+    cy.focused().then($el => {
+      expect($el).to.contain("Invoices");
+    });
+  });
+
+  it("focuses the last sub menu item after opening and then pressing up", () => {
+    cy.contains("Dashboard").click();
+
+    expect(cy.contains("Customers")).to.exist;
+    cy.focused().then($el => {
+      expect($el).to.contain("Customers");
+    });
+    cy.contains("Customers").type("{uparrow}");
+    cy.focused().then($el => {
+      expect($el).to.contain("Carriers");
+    });
+  });
 });
