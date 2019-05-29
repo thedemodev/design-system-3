a3428853285e502aa8b238200e28bb025367c4b2
Matt Dunn <mattd@nulogy.com>
2019-05-29 13:27:45 -0400

    alert styles for nikola to extract

--- components/src/Card/Card.js
+++ components/src/Card/Card.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { Box } from "../Box";
+import theme from "../theme";
 
 const Card = styled(Box)({});
 
@@ -11,10 +12,12 @@ Card.propTypes = {
 Card.defaultProps = {
   children: [],
   borderRadius: "medium",
-  boxShadow: "small",
+  // boxShadow: "small",
   bg: "whiteGrey",
   p: "x2",
-  position: "relative"
+  position: "relative",
+  border: "2px solid #000",
+  background: theme.colors.blue
 };
 
 export default Card;
--- components/src/Card/Card.story.js
+++ components/src/Card/Card.story.js
@@ -4,14 +4,74 @@ import { Box } from "../Box";
 import { Flex } from "../Flex";
 import { Text, SubsectionTitle } from "../Type";
 import { Card, CardSet, CPCard } from ".";
+import { Link } from "../Link";
+import { Icon } from "../Icon";
+
 import { DropdownMenu, DropdownButton } from "../DropdownMenu";
 
 storiesOf("Card", module)
   .add("Card", () => <Card>I am a card.</Card>)
   .add("Custom card", () => (
-    <Card bg="black" color="white" borderRadius="small" p="x1">
-      I am a custom card.
-    </Card>
+    <>
+      <Box borderRadius="medium" p="x2" bg="#0080591f" borderLeft="4px solid #008059" mb="x2">
+        <Flex style={{ position: "relative" }}>
+          <Box mr="x1">
+            <Icon size="32" icon="check" color="#008059" />
+          </Box>
+          <Box mt="4px" mb="4px">
+            <Text mt="0" fontWeight="bold">
+              Alert title
+            </Text>
+            <Text>
+              I am an alert. You can <Link color="black">do something about it</Link>.
+            </Text>
+          </Box>
+          <Icon icon="close" size="16" style={{ position: "absolute", top: "8px", right: "8px" }} />
+        </Flex>
+      </Box>
+      <Box
+        width="400px"
+        borderRadius="small"
+        boxShadow="small"
+        p="x2"
+        bg="#0080591f"
+        borderLeft="4px solid #008059"
+        mb="x2"
+      >
+        <Flex style={{ position: "relative" }}>
+          <Box width="32px">
+            <Icon icon="error" color="#008059" />
+          </Box>
+          <Box>
+            <Text mt="0" fontWeight="bold">
+              Alert title
+            </Text>
+            <Text>
+              I am an alert. You can <Link color="black">do something about it</Link>.
+            </Text>
+          </Box>
+          <Icon icon="close" size="16" style={{ position: "absolute", top: "0px", right: "0px" }} />
+        </Flex>
+      </Box>
+      <Box p="x2" bg="lightBlue" borderLeft="4px solid #216BEB" mb="x2">
+        <Text mt="0" fontWeight="bold">
+          Alert title
+        </Text>
+        <Text>I am an alert.</Text>
+      </Box>
+      <Box p="x2" bg="#cc14392e" borderLeft="4px solid #cc1439" mb="x2">
+        <Text mt="0" fontWeight="bold">
+          Alert title
+        </Text>
+        <Text>I am an alert.</Text>
+      </Box>
+      <Box p="x2" bg="#ffbb004f" borderLeft="4px solid #cc14392e">
+        <Text mt="0" fontWeight="bold">
+          Alert title
+        </Text>
+        <Text>I am an alert.</Text>
+      </Box>
+    </>
   ))
   .add("Cardset", () => (
     <CardSet>
