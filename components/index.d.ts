7ae4b06bfb96ded1703af5d1c39bd723bd850d6f
Evan Brodie <evanb@nulogy.com>
2019-06-10 10:12:32 -0400

    Moves imports to top level of declaration file

--- components/index.d.ts
+++ components/index.d.ts
@@ -1,21 +1,21 @@
-declare module "@nulogy/components" {
-  import * as React from "react";
-  import {
-    ColorProps,
-    SpaceProps,
-    WidthProps,
-    MaxWidthProps,
-    BorderRadiusProps,
-    BoxShadowProps,
-    TextAlignProps,
-    OrderProps,
-    HeightProps,
-    MinHeightProps,
-    MaxHeightProps,
-    PositionProps,
-    DisplayProps
-  } from "styled-system";
+import * as React from "react";
+import {
+  ColorProps,
+  SpaceProps,
+  WidthProps,
+  MaxWidthProps,
+  BorderRadiusProps,
+  BoxShadowProps,
+  TextAlignProps,
+  OrderProps,
+  HeightProps,
+  MinHeightProps,
+  MaxHeightProps,
+  PositionProps,
+  DisplayProps
+} from "styled-system";
 
+declare module "@nulogy/components" {
   // Mimics how children can be passed to styled components
   interface StyledChildrenProps {
     children?: React.ReactNode;
