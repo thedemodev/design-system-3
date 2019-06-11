acec0ad968e057c85f05aa08c0571d6691799a93
Evan Brodie <evanb@nulogy.com>
2019-06-11 10:16:46 -0400

    Adds declarations for Button components

--- components/index.d.ts
+++ components/index.d.ts
@@ -37,3 +37,18 @@ interface BoxProps
     StyledChildrenProps {}
 
 export class Box extends React.Component<BoxProps> {}
+
+type IconSide = "left" | "right";
+type ButtonSize = "small" | "medium" | "large";
+interface ButtonProps extends SpaceProps, StyledChildrenProps {
+  disabled?: boolean;
+  fullWidth?: boolean;
+  icon?: string;
+  iconSide?: IconSide;
+  size?: ButtonSize;
+  [prop: string]: any; // TODO: Find a better way to allow attributes for HTML <button> element
+}
+export class Button extends React.Component<ButtonProps> {}
+export class PrimaryButton extends Button {}
+export class DangerButton extends Button {}
+export class QuietButton extends Button {}
---
+++ typescript-storybook/src/Button.story.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import {
+  Button,
+  PrimaryButton,
+  DangerButton,
+  QuietButton
+} from "@nulogy/components";
+
+storiesOf("Buttons", module)
+  .add("Button", () => <Button>Create project</Button>)
+  .add("PrimaryButton", () => <PrimaryButton>Create project</PrimaryButton>)
+  .add("DangerButton", () => <DangerButton>Delete project</DangerButton>)
+  .add("QuietButton", () => <QuietButton>Create project</QuietButton>)
+  .add("With a selected size", () => (
+    <React.Fragment>
+      <Button size="small">Create project</Button>
+      <Button size="medium">Create project</Button>
+      <Button size="large">Create project</Button>
+    </React.Fragment>
+  ))
+  .add("With a selected Icon", () => (
+    <React.Fragment>
+      <Button icon="add" iconSide="left">
+        Create project
+      </Button>
+      <Button icon="add" iconSide="right">
+        Create project
+      </Button>
+    </React.Fragment>
+  ))
+  .add("Set to full width", () => (
+    <PrimaryButton fullWidth>Create project</PrimaryButton>
+  ))
+  .add("Set to disabled", () => (
+    <PrimaryButton disabled>Create project</PrimaryButton>
+  ))
+  .add("As a link", () => (
+    <PrimaryButton as="a" href="/">
+      Create project
+    </PrimaryButton>
+  ));
