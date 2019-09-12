e5318c1b1a59cf38a6772eeb811733da7ea68f45
Clemens Park <clemensp@nulogy.com>
2019-09-12 16:22:37 -0400

    WIP: row expansion
    Didn't use the grid API, so it doesn't update.
    But it renders a second row...

--- components/src/Table/Table.js
+++ components/src/Table/Table.js
@@ -12,6 +12,15 @@ const NameCellRenderer = () => (
   </div>
 );
 
+// const handleToggle = () => console.log("toggleD!");
+
+const Expander = props =>
+  console.log("hello", props) || (
+    <div onClick={e => console.log("expander click event", e) || props.onToggle(props.data.id)}>
+      {props.getToggleStatus(props.data.id) ? "v" : ">"}
+    </div>
+  );
+
 class Table extends React.Component {
   constructor(props) {
     super(props);
@@ -22,7 +31,12 @@ class Table extends React.Component {
           field: "id",
           checkboxSelection: true,
           headerCheckboxSelection: true,
-          pinned: true
+          pinned: "left"
+        },
+        {
+          cellRendererFramework: cellProps => (
+            <Expander getToggleStatus={this.rowExpanded} onToggle={this.toggleRowExpanded} {...cellProps} />
+          )
         },
         {
           headerName: "Make",
@@ -51,7 +65,13 @@ class Table extends React.Component {
           id: 1,
           make: "Toyota very long cell that will have to wrap and do things for sure",
           model: "Celica",
-          price: 35000
+          price: 35000,
+          expanded: false
+        },
+        {
+          expanded: false,
+          expandableRow: true,
+          parentId: 1
         },
         {
           id: 2,
@@ -67,14 +87,45 @@ class Table extends React.Component {
         }
       ],
       rowHeight: 56,
-      headerHeight: 56
+      headerHeight: 56,
+      rowExpanded: {}
     };
+
+    this.rowExpanded = this.rowExpanded.bind(this);
+    this.toggleRowExpanded = this.toggleRowExpanded.bind(this);
+  }
+
+  rowExpanded(parentRowId) {
+    return this.state.rowExpanded[parentRowId];
+  }
+
+  toggleRowExpanded(parentRowId) {
+    console.log("hello??", this.state, this.state.rowExpanded);
+    const newState = Object.assign({}, this.state.rowExpanded, { [parentRowId]: !this.state.rowExpanded[parentRowId] });
+    this.setState({ rowExpanded: newState });
   }
 
   render() {
+    console.log("rerender", this.state);
     return (
       <div className="ag-theme-balham">
         <AgGridReact
+          isFullWidthCell={rowNode => {
+            console.log("row node", rowNode);
+            return rowNode.data.expandableRow;
+          }}
+          fullWidthCellRendererFramework={
+            props => (
+              <div onClick={props => console.log("click!", props)}>
+                {(!!this.rowExpanded(props.data.parentId)).toString()}
+              </div>
+            )
+
+            // props => <div onClick={props => console.log("click!", props)}>expandable row</div>
+            // console.log(props) || this.rowExpanded(props.data.parentId) ? (
+            //   <div onClick={() => console.log("clicky") || this.toggleRowExpanded(props.data.parentId)}>expanded</div>
+            // ) : null
+          }
           rowSelection="multiple"
           columnDefs={this.state.columnDefs}
           rowData={this.state.rowData}
